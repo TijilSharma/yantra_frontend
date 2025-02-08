d81ec41504da38157f215ea04dc5775696837391
Rajesh Khurana <rajeshkhurana@Rajeshs-MacBook-Pro.local>
2025-02-08 23:24:21 +0530

    Access-Granted

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/load-data": {
+        target: "https://predictivemain.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
+
